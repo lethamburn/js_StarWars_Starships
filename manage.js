32945aec933edf660ada90fe68c03e27b12b05dd
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-05-28 17:13:23 +0200

    Update manage.js

--- manage.js
+++ manage.js
@@ -1,6 +1,6 @@
 const form$$ = document.querySelector("form");
 
-form$$ && form$$.addEventListener("submit", (sub) => {
+form$$.addEventListener("submit", (sub) => {
   sub.preventDefault();
 
   const name = form$$.querySelector(".starshipname").value;
@@ -20,12 +20,33 @@ form$$ && form$$.addEventListener("submit", (sub) => {
   addProduct(newProduct);
 });
 
+form$$.addEventListener("change", (sub) => {
+  sub.preventDefault();
+
+  const name = form$$.querySelector(".starshipname").value;
+  const class1 = form$$.querySelector(".class1").value;
+  const stars = form$$.querySelector(".stars").value;
+  const description = form$$.querySelector(".description").value;
+  const image = form$$.querySelector(".image").value;
+
+  const newProduct = {
+    name: name,
+    class1: class1,
+    stars: stars,
+    description: description,
+    image: image,
+  };
+
+  previewProduct(newProduct);
+});
+
 function addProduct(newProduct) {
   fetch("http://localhost:3000/ships/", {
     method: "POST",
     headers: { "Content-type": "application/json; charset=UTF-8" },
     body: JSON.stringify(newProduct),
-  })}
+  });
+}
 /*    .then((res) => res.json())
     .then((resData) => {
       console.log(resData);
